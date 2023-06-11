c09b425a974c082534b56c0e0f5619e586567efc
itzandroidtab <koen@itzandroidtab.nl>
2023-06-11 15:47:24 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <klib/klib.hpp>
+#include <klib/delay.hpp>
+#include <klib/stream.hpp>
+#include <klib/usb/device/mouse.hpp>
+
+#include <io/can.hpp>
+#include <io/usb.hpp>
+#include <io/system.hpp>
+#include <io/power.hpp>
+#include <io/pins.hpp>
+
+namespace target = klib::target;
+
+int main() {
+    // enable tracing on the hardware
+    target::io::system::trace::enable<true>();
+    
+    klib::cout << "Booting" << klib::endl;
+
+    // configure the usb pll
+    target::io::system::clock::set_usb<12'000'000>();
+
+    // get the usb hardware
+    using usb = target::io::usb<target::io::periph::lqfp_80::usb0, klib::usb::device::mouse_hid, target::irq>;
+
+    // init the usb hardware
+    usb::init();
+
+    while (!usb::device::is_configured<usb>()) {
+        klib::delay<klib::io::systick>(klib::time::ms(10));
+        klib::cout << ".";
+    }
+    klib::cout << klib::endl;
+    klib::cout << "Connected to host" << klib::endl;
+
+    while (true) {
+        klib::cout << "Sending move command" << klib::endl;
+        usb::device::write<usb>(0x00, -1, 0);
+        usb::device::write<usb>(0x00, 1, 0);
+
+        klib::delay<klib::io::systick>(klib::time::s{10});
+    }
+}
